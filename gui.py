46c97361fbfab7d6ef31665469903c8eb4cee571
SoggyBuket <55370006+SoggyBuket@users.noreply.github.com>
2021-08-18 21:26:49 -0700

    add comments and small things to gui.py

--- gui.py
+++ gui.py
@@ -1,13 +1,18 @@
 import tkinter as tk
 
 class Application(tk.Frame):
+    """A class for the main window"""
+
     def __init__(self, master=None):
+        """Initialize basic variables and call methods"""
         super().__init__(master)
         self.master = master
+        # pack the current frame into the master container (the window)
         self.pack()
         self.create_widgets()
 
     def create_widgets(self):
+        """Create the base widgets the window has on it"""
         self.hi_there = tk.Button(self, command=self.say_hi)
         self.hi_there["text"] = "Hello world\n(click me)"
         self.hi_there.pack(side="top")
@@ -18,6 +23,13 @@ class Application(tk.Frame):
     def say_hi(self):
         print("Hello :)")
 
+# initialize the window/container
 root = tk.Tk()
 app = Application(master=root)
+
+# use the window manager to set the title and size of the window
+app.master.title("Sudoku Solver")
+app.master.maxsize(1000, 400)
+
+# start the program/window stuff
 app.mainloop()
