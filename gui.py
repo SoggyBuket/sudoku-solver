44846e68cec23bca9a1a73471e3ded66c01dc63e
SoggyBuket <55370006+SoggyBuket@users.noreply.github.com>
2021-08-19 18:45:52 -0700

    delete all of the old code from gui.py
    Most of it was an outdated way of using tkinter and I want the most modern I can do

--- gui.py
+++ gui.py
@@ -1,45 +1,7 @@
 import tkinter as tk
+from tkinter import ttk
 
-class Application(tk.Frame):
-    """A class for the main window"""
-
-    def __init__(self, master=None):
-        """Initialize basic variables and call methods"""
-        super().__init__(master)
-        self.master = master
-        self.master.geometry("600x400")
-        self.master.config(bg="#00154f")
-        # pack the current frame into the master container (the window)
-        self.config(bg="green")
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        """Create the base widgets the window has on it"""
-        self.hi_there = tk.Button(self, fg="blue", command=self.say_hi)
-        # enter is mouse down?
-        self.hi_there.bind("<Enter>", self.test)
-        self.hi_there["text"] = "Hello world\n(click me)"
-        # I don't know how to use the relief option yet :/
-        self.hi_there["relief"] = "flat"
-        self.hi_there.pack(side="top")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("Hello :)")
-
-    def test(self, event):
-        event.widget["activeforeground"] = "red"
-
-# initialize the window/container
 root = tk.Tk()
-app = Application(master=root)
+ttk.Button(root, text="Hello World").grid()
+root.mainloop()
 
-# use the window manager to set the title and size of the window
-app.master.title("Sudoku Solver")
-app.master.maxsize(1000, 400)
-
-# start the program/window stuff
-app.mainloop()
