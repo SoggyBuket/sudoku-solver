658e544730ada6ad6047decaad4392e197aa1a38
SoggyBuket <55370006+SoggyBuket@users.noreply.github.com>
2021-08-19 19:59:34 -0700

    update gui.py to start work on the actual window

--- gui.py
+++ gui.py
@@ -1,7 +1,16 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import ttk
 
-root = tk.Tk()
-ttk.Button(root, text="Hello World").grid()
-root.mainloop()
+class SudokuGUI:
+    def __init__(self, root):
+        self.root = root
 
+        self.root.title("Sudoku Solver")
+        
+        self.main_frame = ttk.Frame(root, padding="3 3 12 12")
+        self.main_frame.grid(column=0, row=0, sticky=(N, W, E, S))
+
+        # self.button_frame = ttk.Frame(main_frame, padding)
+
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
